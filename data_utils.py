1c0ae426305bea26002831870deb53d29a34a8f5
ziedfakh <65912881+ziedfakh@users.noreply.github.com>
2021-12-15 23:14:35 +0100

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -46,6 +46,8 @@ class TextMelLoader(torch.utils.data.Dataset):
             melspec = self.stft.mel_spectrogram(audio_norm)
             melspec = torch.squeeze(melspec, 0)
         else:
+            filename=filename.replace("wav","npy")
+            print(filename)
             melspec = torch.from_numpy(np.load(filename))
             assert melspec.size(0) == self.stft.n_mel_channels, (
                 'Mel dimension mismatch: given {}, expected {}'.format(
